2824cf136c6b3dd7dd48623571e8d2a5ae33391e
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-18 03:42:04 -0500

    Split out get_bg() and get_width().

--- button.c
+++ button.c
@@ -12,6 +12,14 @@ static void xstatus_draw_button(struct XSButton * restrict b)
 		xstatus_get_button_gc(b->xc), XSTATUS_CONST_PAD,
 		xstatus_get_font_size().height, b->label);
 }
+static pixel_t get_bg(xcb_connection_t * restrict xc)
+{
+	return jb_get_pixel(xc, xstatus_get_colormap(xc), XSTATUS_BUTTON_BG);
+}
+static uint16_t get_width(uint16_t fw, const char * label)
+{
+	return fw * strlen(label) + fw;
+}
 struct XSButton * xstatus_create_button(xcb_connection_t * restrict xc,
 	const int16_t x, char * label)
 {
@@ -23,10 +31,9 @@ struct XSButton * xstatus_create_button(xcb_connection_t * restrict xc,
 	const struct JBDim f = xstatus_get_font_size();
 	xcb_create_window(xc, XCB_COPY_FROM_PARENT, b->window,
 		xstatus_get_window(xc), b->x = x, 0,
-		b->width = f.w * strlen(label) + f.w,
+		b->width = get_width(f.w, label),
 		f.h + XSTATUS_CONST_PAD, 0, 0, 0, XCB_CW_BACK_PIXEL
-		| XCB_CW_EVENT_MASK, (uint32_t[]){jb_get_pixel(xc,
-		xstatus_get_colormap(xc), XSTATUS_BUTTON_BG),
+		| XCB_CW_EVENT_MASK, (uint32_t[]){get_bg(xc),
 		XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS});
 	xcb_map_window(xc, b->window);
 	xstatus_draw_button(b);
