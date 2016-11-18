0d52e38e9cf28ec53f1a6f24c4790126a63eeadd
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-18 05:38:27 -0500

    Tightly scoped f.

--- clock.c
+++ clock.c
@@ -9,10 +9,13 @@ uint16_t xstatus_draw_clock(xcb_connection_t * xc)
 	char buf[30];
 	const size_t sz = strftime(buf, sizeof buf,
 		XSTATUS_TIME_FORMAT, localtime(&(time_t){time(NULL)}));
-	const struct JBDim f = xstatus_get_font_size();
-	const uint16_t offset = xstatus_get_screen(xc)->width_in_pixels
-		- f.w * sz;
-	xcb_image_text_8(xc, sz, xstatus_get_window(xc),
-		xstatus_get_gc(xc), offset, f.h, buf);
+	uint16_t offset;
+	{ // f scope
+		const struct JBDim f = xstatus_get_font_size();
+		offset = xstatus_get_screen(xc)->width_in_pixels
+			- f.w * sz;
+		xcb_image_text_8(xc, sz, xstatus_get_window(xc),
+			xstatus_get_gc(xc), offset, f.h, buf);
+	}
 	return offset;
 }
