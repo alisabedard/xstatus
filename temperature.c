d95a2b00d4fb8f00865295192b524b3f047a2d96
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-19 14:23:14 -0500

    Make get_temp() static.  Split out format().

--- temperature.c
+++ temperature.c
@@ -4,9 +4,13 @@
 #include "font.h"
 #include "util.h"
 #include <stdio.h>
-uint8_t get_temp(void)
+static uint8_t get_temp(void)
 {
 	return xstatus_system_value(XSTATUS_SYSFILE_TEMPERATURE) / 1000;
+}
+static uint8_t format(char * restrict buf, const uint8_t sz)
+{
+	return snprintf(buf, sz, "%dC", get_temp());
 }
 // Returns x offset for next item
 uint16_t draw_temp(xcb_connection_t * xc, const uint16_t offset)
@@ -16,7 +20,7 @@ uint16_t draw_temp(xcb_connection_t * xc, const uint16_t offset)
 	const int16_t x = offset + XSTATUS_CONST_PAD;
 	{ // buf scope
 		char buf[sz];
-		sz = snprintf(buf, sz, "%dC", get_temp());
+		sz = format(buf, sz);
 		xcb_image_text_8(xc, sz, xstatus_get_window(xc),
 			xstatus_get_gc(xc), x, f.h, buf);
 	}
