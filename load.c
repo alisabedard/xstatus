00dd382cf8455ba38ee5d3f0a278661ed5fb8f9f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-19 04:37:41 -0500

    Reduced the width of variable sz.  Split out format().

--- load.c
+++ load.c
@@ -4,19 +4,21 @@
 #include "font.h"
 #include <stdio.h>
 #include <stdlib.h>
+uint8_t format(char * restrict buf, const uint8_t sz)
+{
+	double l;
+	getloadavg(&l, 1);
+	return snprintf(buf, sz, "%.2f", l);
+}
 // Returns x offset for next item
 uint16_t xstatus_draw_load(xcb_connection_t * xc, const uint16_t offset)
 {
 	const struct JBDim f = xstatus_get_font_size();
-	uint16_t sz=6;
+	uint8_t sz=6;
 	const int16_t x = offset + XSTATUS_CONST_PAD;
 	{ // buf scope
 		char buf[sz];
-		{ // l scope
-			double l;
-			getloadavg(&l, 1);
-			sz = snprintf(buf, sz, "%.2f", l);
-		}
+		sz = format(buf, sz);
 		xcb_image_text_8(xc, sz, xstatus_get_window(xc),
 			xstatus_get_gc(xc), x, f.h, buf);
 	}
