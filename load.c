f85e8d84f3f3c9f052097cff6cfe84265f4ebb46
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-19 04:32:46 -0500

    Tightly scoped buf in xstatus_draw_load().  Saved value of x position of widget to use in render and in return.

--- load.c
+++ load.c
@@ -9,11 +9,14 @@ uint16_t xstatus_draw_load(xcb_connection_t * xc, const uint16_t offset)
 {
 	double l[1];
 	getloadavg(l, 1);
-	uint16_t sz=6;
-	char buf[sz];
-	sz = snprintf(buf, sz, "%.2f", l[0]);
 	const struct JBDim f = xstatus_get_font_size();
-	xcb_image_text_8(xc, sz, xstatus_get_window(xc), xstatus_get_gc(xc),
-		offset + XSTATUS_CONST_PAD, f.h, buf);
-	return f.w * sz + offset + XSTATUS_CONST_WIDE_PAD;
+	uint16_t sz=6;
+	const int16_t x = offset + XSTATUS_CONST_PAD;
+	{ // buf scope
+		char buf[sz];
+		sz = snprintf(buf, sz, "%.2f", l[0]);
+		xcb_image_text_8(xc, sz, xstatus_get_window(xc),
+			xstatus_get_gc(xc), x, f.h, buf);
+	}
+	return x + f.w * sz + XSTATUS_CONST_PAD;
 }
