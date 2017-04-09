2eacc3e243cdc30ad9c66e3bea75f0cbc397c75f
Jeffrey E. Bedard <jefbed@gmail.com>
2017-04-09 01:41:52 -0400

    Shortened of buffer size enumeration.  Broke long line.

--- util.c
+++ util.c
@@ -10,18 +10,19 @@ void xstatus_create_gc(xcb_connection_t * xc, const xcb_gcontext_t gc,
 	const char * restrict bg)
 {
 	jb_create_gc(xc, gc, win, fg, bg);
-	xcb_change_gc(xc, gc, XCB_GC_FONT, &(uint32_t){xstatus_get_font(xc)});
+	xcb_change_gc(xc, gc, XCB_GC_FONT,
+		&(uint32_t){xstatus_get_font(xc)});
 }
 // returns -1 on error
 int32_t xstatus_system_value(const char *filename)
 {
-	enum { XSTATUS_SYSTEM_VALUE_BUFFER_SIZE = 8 };
-	char buf[XSTATUS_SYSTEM_VALUE_BUFFER_SIZE];
+	enum { BUFSZ = 8 };
+	char buf[BUFSZ];
 	{ // f scope
 		fd_t f = jb_open(filename, O_RDONLY);
 		if (f == -1)
 			return -1;
-		read(f, buf, XSTATUS_SYSTEM_VALUE_BUFFER_SIZE);
+		read(f, buf, BUFSZ);
 		close(f);
 	}
 	return atoi(buf);
