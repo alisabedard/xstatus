c15b0285ec4c883c3b8547d2d47864746ff4306b
Jeffrey E. Bedard <jefbed@gmail.com>
2017-06-03 12:56:17 -0400

    Defined accessor generator macros XDATA_CACHED and XDATA_GETID.

--- xdata.c
+++ xdata.c
@@ -1,28 +1,15 @@
 // Copyright 2017, Jeffrey E. Bedard
 #include "xdata.h"
 #include "libjb/xcb.h"
-xcb_screen_t * xstatus_get_screen(xcb_connection_t * xc)
-{
-	static xcb_screen_t * s;
-	return s ? s : (s = jb_get_xcb_screen(xc));
+#define XDATA_CACHED(type, name, value) type name(xcb_connection_t * xc) \
+{\
+	static type a; \
+	return a ? a : (a = value); \
 }
-xcb_colormap_t xstatus_get_colormap(xcb_connection_t * xc)
-{
-	static xcb_colormap_t c;
-	return c ? c : (c = xstatus_get_screen(xc)->default_colormap);
-}
-xcb_gcontext_t xstatus_get_gc(xcb_connection_t * xc)
-{
-	static xcb_gcontext_t g;
-	return g ? g : (g = xcb_generate_id(xc));
-}
-xcb_gcontext_t xstatus_get_button_gc(xcb_connection_t * xc)
-{
-	static xcb_gcontext_t g;
-	return g ? g : (g = xcb_generate_id(xc));
-}
-xcb_gcontext_t xstatus_get_invert_gc(xcb_connection_t * xc)
-{
-	static xcb_gcontext_t g;
-	return g ? g : (g = xcb_generate_id(xc));
-}
+#define XDATA_GETID(type, name) XDATA_CACHED(type, name, xcb_generate_id(xc))
+XDATA_CACHED(xcb_screen_t *, xstatus_get_screen, jb_get_xcb_screen(xc));
+XDATA_CACHED(xcb_colormap_t, xstatus_get_colormap,
+	xstatus_get_screen(xc)->default_colormap);
+XDATA_GETID(xcb_gcontext_t, xstatus_get_gc);
+XDATA_GETID(xcb_gcontext_t, xstatus_get_button_gc);
+XDATA_GETID(xcb_gcontext_t, xstatus_get_invert_gc);
