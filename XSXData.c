3a060934de9402ecf928c1054309df97d8dec6f4
Jeffrey E. Bedard <jefbed@gmail.com>
2020-07-07 11:19:14 -0400

    Renamed file.

--- XSXData.c
+++ XSXData.c
@@ -12,14 +12,16 @@ void init_XSXData(xcb_connection_t * xc, struct XSXData * i) {
   i->invert_gc = xcb_generate_id(xc);
   i->window = xcb_generate_id(xc);
 }
+#if 0
 #define XDATA_CACHED(type, name, value) type name(xcb_connection_t * xc) \
-	{ static type a; return a ? a : (a = value); }
+{ static type a; return a ? a : (a = value); }
 #define XDATA_GETID(type, name) XDATA_CACHED(type, name, xcb_generate_id(xc))
 XDATA_CACHED(xcb_screen_t *, xstatus_get_screen, jb_get_xcb_screen(xc));
 XDATA_CACHED(xcb_colormap_t, xstatus_get_colormap,
-	xstatus_get_screen(xc)->default_colormap);
+  xstatus_get_screen(xc)->default_colormap);
 XDATA_GETID(xcb_gcontext_t, xstatus_get_gc);
 XDATA_GETID(xcb_gcontext_t, xstatus_get_button_gc);
 XDATA_GETID(xcb_gcontext_t, xstatus_get_invert_gc);
 XDATA_GETID(xcb_window_t,  xstatus_get_window);
 XDATA_GETID(xcb_font_t,  xstatus_get_font);
+#endif//0
--- XSXData.h
+++ XSXData.h
@@ -11,6 +11,7 @@ struct XSXData {
   xcb_window_t window;
 };
 void init_XSXData(xcb_connection_t * xc, struct XSXData * i);
+#if 0
 xcb_colormap_t xstatus_get_colormap(xcb_connection_t * xc);
 xcb_gcontext_t xstatus_get_button_gc(xcb_connection_t * xc);
 xcb_gcontext_t xstatus_get_gc(xcb_connection_t * xc);
@@ -18,4 +19,5 @@ xcb_gcontext_t xstatus_get_invert_gc(xcb_connection_t * xc);
 xcb_screen_t * xstatus_get_screen(xcb_connection_t * xc);
 xcb_window_t xstatus_get_window(xcb_connection_t * xc);
 xcb_font_t xstatus_get_font(xcb_connection_t * xc);
+#endif//0
 #endif//!XS_XDATA_H
